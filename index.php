b7adafd13d3e09900b7432dbd18fc7f1107c74a9
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-18 11:24:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+	<ul>
+		<li><a href="show.php">Show</a></li>
+		<li><a href="form_create.php">Create</a></li>
+		<li><a href="form_delete.php">Delete</a></li>
+		<li><a href="form_update.php">Update</a></li>
+	</ul>
+</html>
