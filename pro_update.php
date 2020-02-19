06f13cdc684167d0f27ae713becd31ef105f547a
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-19 22:52:21 +0800

    Update pro_update.php

--- pro_update.php
+++ pro_update.php
@@ -1,5 +1,5 @@
 <?php
-$id = $_GET['id'];
+$id = $_POST['id'];
 $name = $_POST['name'];
 $price = $_POST['price'];
 $description = $_POST['description'];
@@ -9,7 +9,7 @@ $date = '2019-06-01 00:35:07';
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => "http://rdapi.herokuapp.com/product/create.php",
+    CURLOPT_URL => "http://rdapi.herokuapp.com/product/update.php",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -17,8 +17,9 @@ curl_setopt_array($curl, array(
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS =>"{\n    \"name\" : \"$name\",\n    \"price\" : \"$price\",\n    \"description\" : \"$description\",\n    \"category_id\" : $category,\n    \"created\" : \"$date\"\n}",
+    CURLOPT_POSTFIELDS =>"{\n    \"id\" : \"$id\",\n    \"name\" : \"$name\",\n    \"price\" : \"$price\",\n    \"description\" : \"$description\",\n    \"category_id\" : $category,\n    \"created\" : \"$date\"\n}",
     CURLOPT_HTTPHEADER => array(
+      "Content-Type: application/json",
       "Content-Type: text/plain"
     ),
   ));
