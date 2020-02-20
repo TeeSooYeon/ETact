e4b9f1487a00a83bd38b1f57892ef24c8eb2624a
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-20 11:06:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<ul>
-		<li><a href="show.php">Show</a></li>
+		<li><a href="showlist.php">Show</a></li>
 		<li><a href="form_create.php">Create</a></li>
 		<li><a href="form_delete.php">Delete</a></li>
 		<li><a href="form_update.php">Update</a></li>
