aca8e6d3aa21ef55bb6ab5b68d1515c0b8b8d70f
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-20 11:06:03 +0800

    Create showlist.php

---
+++ showlist.php
@@ -0,0 +1,32 @@
+<?php
+ $json = file_get_content('http://api-act.herokuapp.com/show_data.php');
+ $data = json_decode($json,true);
+ 
+ $list = $data['records'];
+?>
+<html>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Category</th>
+      <th>Description</th>
+    </tr>
+    <?php
+      foreach($list as $result){
+        $name = $result['name'];
+        $price = $result['price'];
+        $desc = $result['description'];
+        $cat = $result['category_name'];
+    ?>
+    <tr>
+      <td> <?php echo $name ;?> </td>
+      <td><?php echo $price ;?></td>
+      <td><?php echo $cat ;?></td>
+      <td><?php echo $desc ;?></td>
+    </tr>
+    <?php
+      }
+    ?>
+  </table>
+</html>
