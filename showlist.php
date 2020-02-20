e21999bddf93d953bd0206cf28bf194943173a83
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-20 11:34:45 +0800

    Update showlist.php

--- showlist.php
+++ showlist.php
@@ -16,7 +16,7 @@
       foreach($list as $result){
     ?>
     <tr>
-     <td> <a href="product_profile.php&id=<?php echo $result['id'];?>"> <?php echo $result['name']; ?> </a> </td>
+     <td> <a href="index.php?page=show_product&id=<?php echo $result['id'];?>"> <?php echo $result['name']; ?> </a> </td>
       <td><?php echo $result['price']; ?> </td>
       <td><?php echo $result['description']; ?> </td>
       <td><?php echo $result['category_name'];?> </td>
