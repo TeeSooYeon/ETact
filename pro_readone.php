0fe7f0d7fe45c7128ca7e0c5a8d24dad0b20917c
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-20 10:52:01 +0800

    Create pro_readone.php

---
+++ pro_readone.php
@@ -0,0 +1,24 @@
+<?php
+$id = $_POST['id'];
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://rdapi.herokuapp.com/product/read_one.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS =>"{\n    \"id\" : \"$id\",\n    \"name\" : \"$name\",\n    \"price\" : \"$price\",\n    \"description\" : \"$description\",\n    \"category_id\" : $category,\n    \"created\" : \"$date\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json",
+    "Content-Type: text/plain"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
