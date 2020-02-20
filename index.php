4fff12f0a8338eadd4325dbc03744742b82ea7a2
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-20 11:30:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
+<?php
+$load = (isset($_GET['page'])&& $_GET['page'] !='')? $_GET['page'] : '';
+?>
+
 <html>
 	<ul>
-		<li><a href="showlist.php">Show</a></li>
-		<li><a href="form_create.php">Create</a></li>
-		<li><a href="form_delete.php">Delete</a></li>
-		<li><a href="form_update.php">Update</a></li>
-		<li><a href="form_readone.php">Find</a></li>
+		<li><a href="index.php?page=list">Show</a></li>
+		<li><a href="index.php?page=create">Create</a></li>
+		<li><a href="index.php?page=update">Delete</a></li>
+		<li><a href="index.php?page=delete">Update</a></li>
 	</ul>
+<?php
+	switch($load){
+		case 'list':
+			require_once('showlist.php');
+		break;
+		case 'show_product':
+			require_once('product_profile.php');
+		break;	
+		case 'create':
+			require_once('form_create.php');
+		break;
+		case 'update':
+			require_once('form_update.php');
+		break;
+		case 'delete':
+			require_once('form_delete.php');
+		break;
+	}
+?>
 </html>
