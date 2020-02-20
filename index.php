9c2714ea604e09238adfff1cc9bfbdc56d35da36
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-20 10:55:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 		<li><a href="form_create.php">Create</a></li>
 		<li><a href="form_delete.php">Delete</a></li>
 		<li><a href="form_update.php">Update</a></li>
+		<li><a href="form_readone.php">Find</a></li>
 	</ul>
 </html>
