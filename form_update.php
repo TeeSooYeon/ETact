9d04fb0ddd4d48a4fdb926f83306c211d2c3fb05
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-02-25 19:37:16 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -14,12 +14,12 @@
 <br/>
 <br/>
 <div class="w3-container">
-<form action="pro_update.php?id=<?php echo $id;?>" method="POST">
+<form action="pro_update.php?id=<?php echo $id ?>" method="POST">
 	Name:<br/><input class="w3-input w3-border w3-round-large" type="text" name="name" value="<?php echo $result['name'];?>"/><br/><br/>
-	Description:<br/><textarea class="w3-input w3-border w3-round-large" name="description" value="<?php echo $result['description']; ?>"/><?php echo $result['description']; ?></textarea><br/><br/>
+	Description:<br/><textarea class="w3-input w3-border w3-round-large" name="description"><?php echo $result['description']; ?></textarea><br/><br/>
 	Price:<br/><input class="w3-input w3-border w3-round-large" type="number" name="price" value="<?php echo $result['price']; ?>"/><br/><br/>
 	Category:<select class="w3-select w3-border" name="category">
-		<option value="<?php echo $cview['id'];?>"><?php echo $result['category_name'];?></option>
+		<option value="<?php echo $result['category_id'];?>"><?php echo $result['category_name'];?></option>
 	<?php
       foreach($category as $cview){
     ?>
